f542c59731dca06709220883d11a9f7f9e879116
home <untuzina92@gmail.com>
2023-04-17 11:55:42 -0500

    remove CukesRunner tag commit

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@ZU"
+        tags = ""
 )
 public class CukesRunner {
 
